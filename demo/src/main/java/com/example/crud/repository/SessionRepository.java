55bed94abaa045f3d2bb8fb9d82e66fdc0aa8306
Genielson <ge.nielson@hotmail.com>
2024-07-21 15:31:34 -0300

    feat(repository): add Session repository

---
+++ demo/src/main/java/com/example/crud/repository/SessionRepository.java
@@ -0,0 +1,12 @@
+package com.example.crud.repository;
+
+import com.example.crud.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> findByStatus(Session.Status status);
+}
